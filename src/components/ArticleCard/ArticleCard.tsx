0ecfaa2397fea7956abb37f1d0e9383255c81faf
Samuel Cornet <corsam28@gmail.com>
2023-07-22 21:10:54 -0500

    Fix some responsiveness issues

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -17,7 +17,6 @@ import GrayInteraction from "../../images/grayInteraction.png";
 import { ArticleCardType } from "../../types/ArticleCardType";
 
 const ArticleCard: FC<ArticleCardType> = ({
-  key,
   title,
   body,
   channel,
@@ -31,113 +30,100 @@ const ArticleCard: FC<ArticleCardType> = ({
   const { h6, size } = typography;
 
   return (
-    <Grid item xs={12} sm={6} md={4} padding={2} key={key}>
-      <Card
-        sx={{
-          maxWidth: 345,
-          margin: "auto",
-          boxShadow: 0,
-          border: `1px solid ${grey["300"]}`,
-          cursor: 'pointer',
-          "&:hover": {
-            boxShadow: 10,
-          },
-        }}
-      >
-        <CardMedia sx={{ height: 190 }} image={image} title={title} />
-        <Grid className={classes.cardTicket}>
-          <Typography
-            fontSize={h6}
-            color={white2.main}
-            mt={-3.3}
-            textTransform="uppercase"
-            component="div"
-            sx={{ paddingLeft: { xs: "2%", sm: 0 } }}
-          >
-            {channel}
+    <Card
+      sx={{
+        maxWidth: 345,
+        margin: "auto",
+        boxShadow: 0,
+        border: `1px solid ${grey["300"]}`,
+        cursor: "pointer",
+        "&:hover": {
+          boxShadow: 10,
+        },
+      }}
+    >
+      <CardMedia sx={{ height: 190 }} image={image} title={title} />
+      <Grid className={classes.cardTicket}>
+        <Typography
+          fontSize={h6}
+          color={white2.main}
+          mt={-3.3}
+          textTransform="uppercase"
+          component="div"
+          sx={{ paddingLeft: { xs: "2%", sm: 0 } }}
+        >
+          {channel}
+        </Typography>
+      </Grid>
+
+      <Grid display="flex" mx={2} mt={1} justifyContent="space-between">
+        <Grid display="flex">
+          <img src={BlancUser} alt="blanc user" width={20} height={20} />
+          <Typography fontSize={size.xs} component="p" pt={0.5} pl={1}>
+            By: <em> {writer}</em>
           </Typography>
         </Grid>
 
-        <Grid display="flex" mx={2} mt={1} justifyContent="space-between">
+        <Grid display="flex">
           <Grid display="flex">
-            <img src={BlancUser} alt="blanc user" width={20} height={20} />
-            <Typography fontSize={size.xs} component="p" pt={0.5} pl={1}>
-              By: <em> {writer}</em>
+            <img
+              src={like === 0 ? GrayHeart : PinkHeart}
+              alt="blanc user"
+              width={20}
+              height={20}
+            />
+            <Typography fontSize={size.xs} component="p" pt={0.5} pl={1} pr={1}>
+              {like}
             </Typography>
           </Grid>
-
-          <Grid display="flex">
-            <Grid display="flex">
-              <img
-                src={like === 0 ? GrayHeart : PinkHeart}
-                alt="blanc user"
-                width={20}
-                height={20}
-              />
-              <Typography
-                fontSize={size.xs}
-                component="p"
-                pt={0.5}
-                pl={1}
-                pr={1}
-              >
-                {like}
-              </Typography>
-            </Grid>
-            |
-            <Grid display="flex" pl={1}>
-              <img
-                src={reply === 0 ? GrayInteraction : OrangeInteraction}
-                alt="blanc user"
-                width={25}
-                height={25}
-              />
-              <Typography fontSize={size.xs} component="p" pt={0.5} pl={1}>
-                {reply}
-              </Typography>
-            </Grid>
+          |
+          <Grid display="flex" pl={1}>
+            <img
+              src={reply === 0 ? GrayInteraction : OrangeInteraction}
+              alt="blanc user"
+              width={25}
+              height={25}
+            />
+            <Typography fontSize={size.xs} component="p" pt={0.5} pl={1}>
+              {reply}
+            </Typography>
           </Grid>
         </Grid>
-        <CardContent>
-          <Typography
-            gutterBottom
-            fontSize={h6}
-            textAlign="left"
-            component="div"
-          >
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary" textAlign="left">
-            {body.substring(0, 166)}...
-          </Typography>
-        </CardContent>
+      </Grid>
+      <CardContent>
+        <Typography gutterBottom fontSize={h6} textAlign="left" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" textAlign="left">
+          {body.substring(0, 166)}...
+        </Typography>
+      </CardContent>
 
-        <CardActions
-          disableSpacing
-          sx={{
-            alignSelf: "stretch",
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "flex-start",
-            p: 0,
+      <CardActions
+        disableSpacing
+        sx={{
+          alignSelf: "stretch",
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "flex-start",
+          p: 0,
+        }}
+      >
+        <Button
+          size="small"
+          style={{
+            textTransform: "none",
+            color: grey["900"],
+            fontSize: 12,
+            paddingRight: 15,
+            marginBottom: 7,
           }}
+          onClick={() => alert("I was clicked!")}
         >
-          <Button
-            size="small"
-            style={{
-              textTransform: "none",
-              color: grey["900"],
-              fontSize: 12,
-              paddingRight: 15,
-              marginBottom: 7,
-            }}
-            onClick={() => alert("I was clicked!")}
-          >
-            Read More
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
+          Read More
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
--- src/views/home/components/Category.tsx
+++ src/views/home/components/Category.tsx
@@ -8,61 +8,59 @@ import { useCategoryStyle } from "../styles/useCategoryStyle";
 import { CategoryType } from "../../../types/CategoryType";
 import { Link } from "react-router-dom";
 
-const Category: FC<CategoryType> = ({ key, channel, image }) => {
+const Category: FC<CategoryType> = ({ channel, image }) => {
   const [show, setShow] = useState(false);
   const { white2, grey } = colors;
   const { h2 } = typography;
   const classes = useCategoryStyle();
 
   return (
-    <Grid item xs={12} sm={6} md={4} padding={2} key={key}>
-      <Card
-        onMouseOver={() => setShow(true)}
-        onMouseOut={() => setShow(false)}
-        sx={{
-          maxWidth: 345,
-          margin: "auto",
-          boxShadow: 0,
-          border: `1px solid ${grey["300"]}`,
-          cursor: "pointer",
-          position: "relative",
-          height: 250,
-          "&:hover": {
-            boxShadow: 10,
-          },
-        }}
-      >
-        <img
-          className={classes.backgroundImage}
-          src={image}
-          alt="Haiti Logo Flag"
-        />
-        {show && (
-          <Grid
-            position="absolute"
-            top="5%"
-            left="5%"
-            width="90%"
-            height={"90%"}
-            bgcolor="#c70505aa"
-            component={Link}
-            to="/channel"
-            sx={{
-              textDecoration: "none",
-            }}
+    <Card
+      onMouseOver={() => setShow(true)}
+      onMouseOut={() => setShow(false)}
+      sx={{
+        maxWidth: 345,
+        margin: "auto",
+        boxShadow: 0,
+        border: `1px solid ${grey["300"]}`,
+        cursor: "pointer",
+        position: "relative",
+        height: 250,
+        "&:hover": {
+          boxShadow: 10,
+        },
+      }}
+    >
+      <img
+        className={classes.backgroundImage}
+        src={image}
+        alt="Haiti Logo Flag"
+      />
+      {show && (
+        <Grid
+          position="absolute"
+          top="5%"
+          left="5%"
+          width="90%"
+          height={"90%"}
+          bgcolor="#c70505aa"
+          component={Link}
+          to="/channel"
+          sx={{
+            textDecoration: "none",
+          }}
+        >
+          <Typography
+            fontSize={h2}
+            component="p"
+            color={white2.main}
+            marginY={10}
           >
-            <Typography
-              fontSize={h2}
-              component="p"
-              color={white2.main}
-              marginY={10}
-            >
-              {channel}
-            </Typography>
-          </Grid>
-        )}
-      </Card>
-    </Grid>
+            {channel}
+          </Typography>
+        </Grid>
+      )}
+    </Card>
   );
 };
 
