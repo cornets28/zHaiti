a3975801ecb955962b9a6140253dc078dc95a243
Samuel Cornet <corsam28@gmail.com>
2023-08-12 10:47:30 -0500

    Refactor the dynamic text section

--- src/views/home/components/DynamicText.tsx
+++ src/views/home/components/DynamicText.tsx
@@ -1,56 +1,25 @@
 import type { FC } from "react";
 import Grid from "@mui/material/Grid";
-import colors from "../../../utils/theme/base/colors";
-import { useTranslation } from 'react-i18next';
+import TextContainer from "./DynamicTextSection/TextContainer/TextContainer";
+import ZhaitiText from "./DynamicTextSection/ZhaitiText/ZhaitiText";
+import ListItem from "./DynamicTextSection/ListItem/ListItem";
+import ListItemContainer from "./DynamicTextSection/ListItemContainer/ListItemContainer";
+import "../styles/useDynamicTextStyle.css";
 
 export const DynamicText: FC = () => {
-  const { t } = useTranslation()
-  const { white2, error } = colors;
-
   return (
-    <Grid
-      container
-      item
-      width="100%"
-      bgcolor={error.focus}
-      overflow='hidden'
-      sx={{
-        textAlign: "center",
-        overflow: "hidden",
-        height: {xs: 300, sm: 500, md: 600}
-      }}
-    >
+    <TextContainer>
       <Grid item className="wrapper">
-        <Grid
-          item
-          sx={{
-            color: white2.main,
-            fontWeight: 400,
-            fontSize: { xs: 30, sm: 45, md: 60 },
-          }}
-        >
-          ZHaiti News
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          className="dynamic-texts"
-          sx={{ height: 50, lineHeight: 50, overflow: "hidden", pt: 50, marginX: 'auto' }}
-        >
+        <ZhaitiText />
+        <ListItemContainer>
           <ul>
-            <li>
-              <span>{t("Bay Nouvèl San Fòs Kote")}</span>
-            </li>
-            <li>
-              <span>{t("Bay Nouvèl Nan Tout Sektè")}</span>
-            </li>
-            <li>
-              <span>{t("Bay Nouvèl Nan Bon Jan Ti Mamit")}</span>
-            </li>
+            <ListItem text="Bay Nouvèl San Fòs Kote" />
+            <ListItem text="Bay Nouvèl Nan Tout Sektè" />
+            <ListItem text="Bay Nouvèl Nan Bon Jan Ti Mamit" />
           </ul>
-        </Grid>
+        </ListItemContainer>
       </Grid>
-    </Grid>
+    </TextContainer>
   );
 };
 
